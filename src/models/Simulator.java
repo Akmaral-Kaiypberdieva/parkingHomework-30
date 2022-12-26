2e8bebcf6136ed8e126e23dd65513bb575322574
Akmaral Kaiypberdieva <yamaralista1@gmail.com>
2022-12-26 18:18:27 +0600

    added dop

--- src/models/Simulator.java
+++ src/models/Simulator.java
@@ -1,7 +1,12 @@
 package models;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summingDouble;
 
 public class Simulator {
     public static void randomNumber(List<Car>cars) {
@@ -22,6 +27,25 @@ public class Simulator {
             }
             day++;
         }
-        park.makeCheckForPay();
+         List<Magazine> magazines = park.makeCheckForPay();
+        var checkForDay = magazines.stream()
+                .collect(groupingBy(Magazine::getDay,summingDouble(Magazine::getSum)));
+        var sumMin = magazines.stream()
+                .min(Comparator.comparing(Magazine::getSum))
+                .map(Magazine::getSum);
+        var sum = magazines.stream()
+                .map(Magazine::getSum);
+        var count = magazines.stream()
+                .filter(e->e.getMinuteCheckOut()<30)
+                .count();
+        var theLongest = magazines.stream()
+                .sorted(Comparator.comparing(Magazine::getMinuteCheckOut).reversed());
+        var theLongest2 = magazines.stream()
+                .collect(Collectors.toList());
+        var lind = magazines.stream()
+                .limit(10);
+        var list = magazines.stream()
+                .filter(c -> c.getCar().equals(cars));
+
     }
 }
