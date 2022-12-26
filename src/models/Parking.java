edee99fb6267ce597315298c92b95476f51e1953
Akmaral Kaiypberdieva <yamaralista1@gmail.com>
2022-12-26 16:36:02 +0600

    added class parking and canvas

---
+++ src/models/Parking.java
@@ -0,0 +1,57 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parking {
+    private List <Car> car;
+    private List<Magazine> magazineList;
+
+    public Parking() {
+        car = new ArrayList<>();
+        magazineList = new ArrayList<>();
+    }
+
+    public  List<Magazine> makeCheckForPay(){
+        List<Magazine> payCheck = new ArrayList<>();
+        try {
+            for(int i = 0; i <= magazineList.size(); i++){
+                int sum = 0;
+                int timeOnPark = 0;
+                Magazine magazine = magazineList.get(0);
+                if(magazine.getMinuteCheckOut() - magazine.getMinuteCheckIn() >= 30 || magazine.getMinuteCheckIn() > 720 && magazine.getMinuteCheckOut() >= 30){
+                    if(magazine.getMinuteCheckOut() > 720 && magazine.getMinuteCheckIn() < 720){
+                        timeOnPark = 720 - magazine.getMinuteCheckIn();
+                    } else if (magazine.getMinuteCheckOut() < 720 && magazine.getMinuteCheckIn() < 720) {
+                        timeOnPark = magazine.getMinuteCheckOut() - magazine.getMinuteCheckIn();
+                    } else if (magazine.getMinuteCheckOut() < 720 && magazine.getMinuteCheckIn() > 720) {
+                        timeOnPark = magazine.getMinuteCheckOut();
+                    }
+                }
+                if(timeOnPark != 0){
+                    sum = timeOnPark / 5 * 10;
+                    payCheck.add(new Magazine(magazine.getCar(), sum, magazine.getDay()));
+                }
+            }
+        } catch (IndexOutOfBoundsException e){
+            e.printStackTrace();
+        }
+        return payCheck;
+    }
+
+    public List<Car> getCar() {
+        return car;
+    }
+
+    public List<Magazine> getMagazineList() {
+        return magazineList;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Parking{" +
+                "car=" + car +
+                ", magazineList=" + magazineList;
+    }
+}
