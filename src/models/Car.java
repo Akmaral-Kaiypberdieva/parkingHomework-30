3a3455d48d8fabcdad18aae0b54193c1b12989cf
Akmaral Kaiypberdieva <yamaralista1@gmail.com>
2022-12-26 16:25:19 +0600

    added class car

---
+++ src/models/Car.java
@@ -0,0 +1,43 @@
+package models;
+
+public class Car {
+    private String number;
+    private Magazine magazine;
+    private State state;
+
+    public Car(String number) {
+        this.number = number;
+        state = State.ON_WAY;
+    }
+
+    public void changeState(Parking park, int minute, int day){
+        if(new Random().nextInt(100) + 1 <= 3){
+            switch (state){
+                case ON_WAY:
+                    if(park.getCar().size() < 20){
+                        System.out.println("Car is parking");
+                        state = State.PARKING;
+                        magazine = new Magazine(this, minute, day);
+                        park.getCar().add(this);
+                    } else {
+                        System.out.println("Parking is full");
+                    }
+                    break;
+                case PARKING:
+                    System.out.println("The car leaves the park");
+                    state = State.ON_WAY;
+                    magazine.setMinuteCheckOut(minute);
+                    magazine.setDay(day);
+                    park.getMagazineList().add(magazine);
+                    park.getCar().remove(this);
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "number='" + number + '\'';
+    }
+}
